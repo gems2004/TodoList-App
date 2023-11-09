6b814b77d79d8c4468bed4ddfa4b65d2dea9e998
Edmon <ed.moussa4@gmail.com>
2023-11-10 00:03:47 +0300

    hid the database uri

--- netlify/functions/config/dbConnect.cjs
+++ netlify/functions/config/dbConnect.cjs
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/TodoAppDB");
+    await mongoose.connect(process.env.DATABASE_URI);
   } catch (err) {
     console.log(err);
   }
